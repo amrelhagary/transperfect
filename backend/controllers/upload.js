d64e3bb8722870413ae6c72c2a2c3b15e7a266d3
Amr Elhagary <amr.elhagary@gmail.com>
2019-09-25 03:28:50 +0200

    implement the strings file uploader and parser

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -1,5 +1,55 @@
 'use strict';
+const StringsModel = require('../models/strings');
+const readline = require('../libs/readline');
+const path = require('path');
 
 exports.uploadStringFile = async (req, res) => {
-    res.send('test')
+    const filename= path.join(__dirname, `/../uploads/${req.file.filename}`);
+    console.log(filename, `upload string file ${req.file.filename}`)
+
+    readlines(filename, (err, lines) => {
+        if (err) {
+            return res.sendStatus(500).send(err.toString());
+        }
+        res.send(`no of lines ${lines}`);
+    });
 };
+
+function readlines(filename, cb) {
+    const rl = readline(filename);
+    let lines = 0
+
+    rl.on('line', (line) => {
+        try {
+            let row = line.split('=', 2).map(t => t.replace(/\s+|\"+|\;/g, '')).filter(s => s != "");
+            if (row.length == 0) {
+                return;
+            }
+
+            let str  = new StringsModel({
+                key: row[0],
+                value: row[1]
+            });
+
+            str
+                .save()
+                .then(() => {
+                    rl.resume();
+                })
+                .catch((err) => {
+                    console.log('Error writing obj to DB at line %s: %s', lines, err.toString())
+                    cb(err, null);
+                });       
+        } catch (e) {
+            console.log('Error parsing file at line %s: %s', lines, e.toString())
+            cb(e, null);
+        };
+        
+        lines++;
+        console.log('Read %s lines.', lines)
+    });
+
+    rl.on('close', () => {
+        cb(null, lines);
+    });
+}
--- backend/libs/readline.js
+++ backend/libs/readline.js
@@ -1,8 +1,30 @@
 'use strict';
 
+const readline = require('readline');
+const Stream = require('stream');
+const fs = require('fs');
 
-function readLines () {
-    // TODO implementation
-}
+function readLines (fileName) {
+    const stream = fs.createReadStream(fileName, {
+      flags: 'r',
+      encoding: 'utf-8',
+      fd: null,
+      bufferSize: 1
+    });
+
+    const outstream = new Stream();
+    const rl = readline.createInterface(stream, outstream);
+    
+    stream.on('error', (e) => {
+      console.log('Error in read-stream: %s', e.toString());
+    })
   
-module.exports = readLines
+    stream.on('end', () => {
+      console.log('Read-Stream ended.');
+      rl.close();
+    })
+  
+    return rl
+  }
+  
+  module.exports = readLines;
